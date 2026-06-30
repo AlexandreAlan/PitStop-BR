0404fcb2d055c5ea3d20b0f14e7cb355d5e28e1b
AlexandreAlan <alexandre.basto444@gmail.com>
2026-06-30 19:45:57 -0300

    src/config/bootstrap.php: carrega config/auth.php pra disponibilizar usuarioAtual()/exigirLogin() em todas as páginas

--- src/config/bootstrap.php
+++ src/config/bootstrap.php
@@ -16,6 +16,7 @@ session_start();
 
 require_once __DIR__ . '/conexao.php';
 require_once __DIR__ . '/csrf.php';
+require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/../includes/functions.php';
 
 $pdo = getConexao();
