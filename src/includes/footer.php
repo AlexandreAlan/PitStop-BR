46aad62c7fb11279ec71029b03e72ac43794c06a
AlexandreAlan <alexandre.basto444@gmail.com>
2026-06-30 20:25:06 -0300

    src/includes/footer.php: bottom-nav ganha Relatórios entre Início e Veículos

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -11,6 +11,9 @@
     <a href="index.php" class="<?= $paginaAtual === 'index.php' ? 'ativo' : '' ?>">
         <i class="bi bi-house-fill"></i>Início
     </a>
+    <a href="relatorios.php" class="<?= $paginaAtual === 'relatorios.php' ? 'ativo' : '' ?>">
+        <i class="bi bi-bar-chart-fill"></i>Relatórios
+    </a>
     <a href="veiculos.php" class="<?= $paginaAtual === 'veiculos.php' ? 'ativo' : '' ?>">
         <i class="bi bi-car-front-fill"></i>Veículos
     </a>
