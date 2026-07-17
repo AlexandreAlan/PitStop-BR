d99644f989e2f047b12f313210eda8735bed44d4
Alexandre Alan <alexandre.basto444@gmail.com>
2026-07-17 16:52:48 -0300

    feat: pre-cache do novo asset foto-comprovante.js no Service Worker

--- src/sw.php
+++ src/sw.php
@@ -33,6 +33,7 @@ const PRECACHE_URLS = [
     '/assets/js/cookies.js',
     '/assets/js/passaporte.js',
     '/assets/js/veiculo_compartilhar.js',
+    '/assets/js/foto-comprovante.js',
     '/assets/img/icon-192.png',
     '/assets/img/icon-512.png',
     '/assets/img/logo-mark.svg',
